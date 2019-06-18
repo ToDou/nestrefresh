ae056a70265d51acc4b341566bdfe5aaa95d1aea
ToDou <yytodou123@gmail.com>
2019-06-18 22:39:54 +0800

    add bintray upload code

--- upload.sh
+++ upload.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 echo "\033[1;32mBuild Start \033[0m"
-./gradlew :shadow:build
+./gradlew :nestrefresh:build
 echo "\033[1;32mInstall Start \033[0m"
-./gradlew :shadow:install
+./gradlew :nestrefresh:install
 echo "\033[1;32mBintray Upload Start \033[0m"
-./gradlew :shadow:bintrayUpload
+./gradlew :nestrefresh:bintrayUpload
