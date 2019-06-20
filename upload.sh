ef253307ccebf435ae341080f8b001145cf1cc41
ToDou <yytodou123@gmail.com>
2019-06-20 22:30:39 +0800

    update upload.sh

--- upload.sh
+++ upload.sh
@@ -3,6 +3,6 @@
 echo "\033[1;32mBuild Start \033[0m"
 ./gradlew :nestrefresh:build
 echo "\033[1;32mInstall Start \033[0m"
-./gradlew :nestrefresh:install
+./gradlew :nestrefresh:publish
 echo "\033[1;32mBintray Upload Start \033[0m"
 ./gradlew :nestrefresh:bintrayUpload
